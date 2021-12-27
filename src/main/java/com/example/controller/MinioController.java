969c3d878a439ca080f86aa704ee854e5a07faa5
hehepeng <18846042826@163.com>
2021-12-27 18:27:42 +0800

    完成minio文件上传删除预览

---
+++ src/main/java/com/example/controller/MinioController.java
@@ -0,0 +1,148 @@
+package com.example.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.example.util.MinioUtil;
+import com.example.util.ResponseResult;
+import com.example.util.ResultCodeEnum;
+import io.minio.messages.Bucket;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Api(tags = "文件管理模块")
+@RestController
+@RequestMapping("/api/file/v1")
+@Slf4j
+public class MinioController {
+
+    @Autowired
+    private MinioUtil minioUtil;
+
+    @ApiOperation(value = "查看存储bucket是否存在")
+    @GetMapping("/bucketExists")
+    public ResponseResult bucketExists(String bucketName) {
+        System.out.println("bucketName: " + minioUtil.bucketExists(bucketName));
+        boolean extised = minioUtil.bucketExists(bucketName);
+        if (!extised) {
+            return ResponseResult.ok().success(false).message("文件不存在");
+        }
+
+        return ResponseResult.ok().message("文件存在");
+    }
+
+    @ApiOperation(value = "创建存储bucket")
+    @GetMapping("/makeBucket")
+    public ResponseResult makeBucket(String bucketName) throws Exception {
+        boolean created = minioUtil.makeBucket(bucketName);
+        System.out.println("minioUtil.makeBucket(bucketName): " + minioUtil.makeBucket(bucketName));
+        if (!created) {
+//            throw new Exception("存储空间已存在");
+            return ResponseResult.ok().success(false).message("存储空间已存在");
+        }
+
+        return ResponseResult.ok().message("创建成功");
+    }
+
+    @ApiOperation(value = "删除存储bucket")
+    @DeleteMapping("/removeBucket")
+    public ResponseResult removeBucket(String bucketName) {
+
+        boolean removed = minioUtil.removeBucket(bucketName);
+
+        if (!removed) {
+            return ResponseResult.ok().success(false).message("删除失败，存储空间不存在");
+        }
+
+        return ResponseResult.ok().message("删除成功");
+    }
+
+    @ApiOperation(value = "获取全部bucket")
+    @GetMapping("/getAllBuckets")
+    public ResponseResult getAllBuckets() {
+        List<Bucket> allBuckets = minioUtil.getAllBuckets();
+        System.out.println("minioUtil.getAllBuckets(): " + allBuckets);
+        Map<String, Object> resultMap = new HashMap<>();
+
+        resultMap.put("bucketsList", allBuckets);
+        System.out.println("resultMap: " + resultMap);
+        return ResponseResult.ok().data(resultMap);
+    }
+
+    @ApiOperation(value = "文件上传")
+    @PostMapping(value = "/upload", consumes = { "multipart/form-data" })
+    @ResponseBody
+    public ResponseResult upload(MultipartFile file) {
+        String fullPath = minioUtil.upload(file);
+        String fileUrl = "";
+        Map<String, Object> resultMap = new HashMap<>();
+
+        if (fullPath == "") {
+            System.out.println("上传失败：" + fullPath);
+            return ResponseResult.error().message("上传失败");
+        }
+
+        fileUrl = "http://101.35.44.70:9001/" + fullPath;
+        resultMap.put("fileUrl", fileUrl);
+
+        return ResponseResult.ok().data(resultMap).message("上传成功");
+    }
+
+    @ApiOperation(value = "图片预览")
+    @GetMapping("/preview")
+    public ResponseResult preview(String fileName) {
+        Map<String, Object> resultMap = new HashMap<>();
+        String previewUrl = minioUtil.preview(fileName);
+
+        System.out.println("preview: " + previewUrl);
+
+        if (previewUrl == "") {
+            return ResponseResult.error().message("图片不存在");
+        }
+
+        resultMap.put("previewUrl:", previewUrl);
+
+        return ResponseResult.ok().data(resultMap);
+    }
+
+    @ApiOperation(value = "文件下载")
+    @GetMapping("/download")
+    public ResponseResult download(String fileName, HttpServletResponse res) {
+        minioUtil.download(fileName, res);
+
+        return null;
+    }
+
+    @ApiOperation(value = "文件删除")
+    @DeleteMapping("/remove")
+    public ResponseResult remove(String fileName) {
+        boolean deleted = minioUtil.remove(fileName);
+
+        if (!deleted) {
+            return ResponseResult.error().message("文件已删除或不存在");
+        }
+
+        return ResponseResult.ok().message("文件删除成功");
+    }
+
+    @ApiOperation(value = "获取所有文件")
+    @GetMapping("/getAllContents")
+    public ResponseResult getAllFiles() {
+
+        minioUtil.listObjects();
+
+        System.out.println("minioUtil.listObjects()：" + minioUtil.listObjects());
+
+        return null;
+    }
+}
---
+++ src/main/java/com/example/util/MinioUtil.java
@@ -0,0 +1,231 @@
+package com.example.util;
+
+import io.minio.*;
+import io.minio.http.Method;
+import io.minio.messages.Bucket;
+import io.minio.messages.DeleteError;
+import io.minio.messages.DeleteObject;
+import io.minio.messages.Item;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FastByteArrayOutputStream;
+import org.springframework.web.multipart.MultipartFile;
+import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 文件服务器工具类
+ */
+@Component
+public class MinioUtil {
+    private String bucketName = "files";
+
+    @Resource
+    private MinioClient minioClient;
+
+    /**
+     * 查看存储bucket是否存在
+     * @return boolean
+     */
+    public Boolean bucketExists(String bucketName) {
+        Boolean found;
+
+        try {
+            found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        } catch (Exception e) {
+            //e.printStackTrace();
+            return false;
+        }
+        return found;
+    }
+
+    /**
+     * 创建存储bucket
+     * @return Boolean
+     */
+    public Boolean makeBucket(String bucketName) {
+        try {
+            minioClient.makeBucket(MakeBucketArgs.builder()
+                    .bucket(bucketName)
+                    .build());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    /**
+     * 删除存储bucket
+     * @return Boolean
+     */
+    public Boolean removeBucket(String bucketName) {
+        try {
+            minioClient.removeBucket(RemoveBucketArgs.builder()
+                    .bucket(bucketName)
+                    .build());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    /**
+     * 获取全部bucket
+     */
+    public List<Bucket> getAllBuckets() {
+        try {
+            return minioClient.listBuckets();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 文件上传
+     * @param file 文件
+     * @return Boolean
+     */
+    public String upload(MultipartFile file) {
+        // 修饰过的文件名 非源文件名
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM/dd");
+        String dirName = dateFormat.format(new Date()) + "/";
+        String fullPath = dirName + file.getOriginalFilename();
+        try {
+            PutObjectArgs objectArgs = PutObjectArgs.builder().bucket(bucketName).object(fullPath)
+                    .stream(file.getInputStream(),file.getSize(),-1).contentType(file.getContentType()).build();
+            //文件名称相同会覆盖
+            minioClient.putObject(objectArgs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+        return fullPath;
+    }
+
+//    /**
+//     * 文件上传
+//     * @param file 文件
+//     * @return Boolean
+//     */
+//    public String upload(MultipartFile file) {
+//        // 修饰过的文件名 非源文件名
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM/dd");
+//        String dirName = dateFormat.format(new Date()) + "/";
+//
+//        String fullPath = dirName + file.getOriginalFilename();
+//
+//        System.out.println("contentType: " + file.getContentType());
+//
+//        try {
+//            PutObjectArgs objectArgs = PutObjectArgs.builder().bucket(bucketName).object(fullPath)
+//                    .stream(file.getInputStream(),file.getSize(),-1).contentType(file.getContentType()).build();
+//
+//            //文件名称相同会覆盖
+//            minioClient.putObject(objectArgs);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return "";
+//        }
+//
+//        return fullPath;
+//    }
+
+    /**
+     * 预览图片
+     * @param fileName 是上传图片的fullPath=>eg:2021-12/27/typora-setup-x64.exe
+     * @return
+     */
+    public String preview(String fileName){
+        try {
+            minioClient.statObject(StatObjectArgs.builder().bucket(bucketName).object(fileName).build());
+
+            return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(bucketName).object(fileName).build());
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    /**
+     * 文件下载
+     * @param fileName 文件名称
+     * @param res response
+     * @return Boolean
+     */
+    public void download(String fileName, HttpServletResponse res) {
+        GetObjectArgs objectArgs = GetObjectArgs.builder().bucket(bucketName)
+                .object(fileName).build();
+        try (GetObjectResponse response = minioClient.getObject(objectArgs)){
+            byte[] buf = new byte[1024];
+            int len;
+            try (FastByteArrayOutputStream os = new FastByteArrayOutputStream()){
+                while ((len=response.read(buf))!=-1){
+                    os.write(buf,0,len);
+                }
+                os.flush();
+                byte[] bytes = os.toByteArray();
+                res.setCharacterEncoding("utf-8");
+                //设置强制下载不打开
+                //res.setContentType("application/force-download");
+                res.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
+                try (ServletOutputStream stream = res.getOutputStream()){
+                    stream.write(bytes);
+                    stream.flush();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 查看文件对象
+     * @return 存储bucket内文件对象信息
+     */
+    public List<Item> listObjects() {
+        Iterable<Result<Item>> results = minioClient.listObjects(
+                ListObjectsArgs.builder().bucket(bucketName).build());
+        List<Item> items = new ArrayList<>();
+        try {
+            for (Result<Item> result : results) {
+                items.add(result.get());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return items;
+    }
+
+    /**
+     * 删除
+     * @param fileName 是上面的fullPath=>eg:2021-12/27/typora-setup-x64.exe
+     * @return
+     * @throws Exception
+     */
+    public boolean remove(String fileName){
+        try {
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(bucketName).object(fileName).build());
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 批量删除文件对象（没测试）
+     * @param objects 对象名称集合
+     */
+    public Iterable<Result<DeleteError>> removeObjects(List<String> objects) {
+        List<DeleteObject> dos = objects.stream().map(e -> new DeleteObject(e)).collect(Collectors.toList());
+        Iterable<Result<DeleteError>> results = minioClient.removeObjects(RemoveObjectsArgs.builder().bucket(bucketName).objects(dos).build());
+        return results;
+    }
+
+}
