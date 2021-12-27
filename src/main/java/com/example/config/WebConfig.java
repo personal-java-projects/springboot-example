02ba36ec4eab7161186ad8d6469088b507005524
hehepeng <cnmsbqnm@gmail.com>
2021-12-28 00:28:13 +0800

    搭建好swagger接口管理框架

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -29,6 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/userLogin").excludePathPatterns("/user/userRegister")
+                // 不拦截swagger相关路径
                 .excludePathPatterns("/swagger**/**").excludePathPatterns("/webjars/**").excludePathPatterns("/v3/**").excludePathPatterns("doc.html");
 
         WebMvcConfigurer.super.addInterceptors(registry);
