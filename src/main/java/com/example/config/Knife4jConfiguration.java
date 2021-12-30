27ff12cfb5a0742936260f4401622b7b4ca16791
hehepeng <18846042826@163.com>
2021-12-30 10:08:41 +0800

    fix knife4j打不开的问题

--- src/main/java/com/example/config/Knife4jConfiguration.java
+++ src/main/java/com/example/config/Knife4jConfiguration.java
@@ -1,8 +1,8 @@
 package com.example.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,14 +11,12 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Configuration
-@EnableSwagger2WebMvc
 public class Knife4jConfiguration {
 
     @Bean
