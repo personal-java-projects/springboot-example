34545b969cfd763c51de3ce122c5618f59dcb9c6
hehepeng <18846042826@163.com>
2022-01-04 16:24:13 +0800

    支持多数据源连接

--- src/main/java/com/example/mapper/RoleMapper.java
+++ src/main/java/com/example/mapper/RoleMapper.java
@@ -2,10 +2,12 @@ package com.example.mapper;
 
 import com.example.pojo.Role;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface RoleMapper {
     // 查询所有角色, 是否根据某个用户
     public List<Role> selectAllRoles();
