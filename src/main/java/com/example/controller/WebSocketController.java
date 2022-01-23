00eac2d401a912abbe7de98a9550e558fb602f95
hehepeng <cnmsbqnm@gmail.com>
2022-01-23 21:08:33 +0800

    重写了minioController上传接口，使用拼接方式获取资源路径（minio存储桶设置成public,链接为永久链接）,新增websocket接口

---
+++ src/main/java/com/example/controller/WebSocketController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import com.example.util.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class WebSocketController {
+    @Autowired
+    private WebSocketServer webSocketServer;
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/webSocket")
+    public ModelAndView socket() {
+        ModelAndView mav=new ModelAndView("/webSocket");
+//        mav.addObject("userId", userId);
+        return mav;
+    }
+
+
+}
