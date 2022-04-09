9c438e70aa882588c17ca649ddccd40e077fee00
hehepeng <cnmsbqnm@gmail.com>
2022-04-09 13:31:36 +0800

    去除原先的定时任务配置

--- src/main/java/com/example/config/ScheduleConfig.java
+++ src/main/java/com/example/config/ScheduleConfig.java
@@ -8,11 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @program: simple-demo
@@ -38,32 +34,4 @@ public class ScheduleConfig {
         taskScheduler.setThreadNamePrefix(scheduleProperties.getThreadNamePrefix());
         return taskScheduler;
     }
-
-//    @Bean
-//    public Executor asyncServiceExecutor() {
-//        logger.info("start asyncServiceExecutor");
-//
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//
-//        // 配置核心线程数
-//        executor.setCorePoolSize(scheduleProperties.getCorePoolSize());
-//        // 配置最大线程数
-//        executor.setMaxPoolSize(scheduleProperties.getMaxPoolSize());
-//        // 配置线程最大空闲时间
-//        executor.setKeepAliveSeconds(scheduleProperties.getKeepAliveSeconds());
-//        // 配置队列大小
-//        executor.setQueueCapacity(scheduleProperties.getQueueCapacity());
-//        // 配置线程池中的线程的名称前缀
-//        executor.setThreadNamePrefix(scheduleProperties.getThreadNamePrefix());
-//
-//        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-//        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
-//        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-//        executor.setWaitForTasksToCompleteOnShutdown(true);
-//
-//        //执行初始化
-//        executor.initialize();
-//
-//        return executor;
-//    }
 }
